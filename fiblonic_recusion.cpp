1ee99e1df35f2e4c0678f3c1ba62fe2a84ec34f6
Deepak <62493802+Deepak-nishad@users.noreply.github.com>
2021-05-06 09:57:05 +0530

    Add files via upload

---
+++ fiblonic_recusion.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include<iostream>
+int fib(int n){
+    if(n == 0)
+    {
+    	return 0;
+	}
+	if(n==1)
+	{
+		return 1;
+	}
+	int output1=fib(n-1);
+	int output2=fib(n-2);
+	return output1+output2;
+	
+}
+int main()
+{
+   cout<<fib(3);
+}
