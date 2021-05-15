c490019f3fb49b4fde30c036ce354e24b799ce6f
Deepak <62493802+Deepak-nishad@users.noreply.github.com>
2021-05-15 10:14:45 +0530

    string basic function

---
+++ string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+int main()
+{
+	/*string *s=new string;
+	*s="deepak";
+	cout<<s<<endl;
+	cout<<*s<<endl;*/
+	
+	string s,s1,s2;
+	s="deepak";
+	s1="manish";
+	 s2=s+s1;
+	cout<<s2<<endl;
+	
+	/*cout<<s.size()<<endl;
+	cout<<s1.size()<<endl;
+	cout<<s2.size()<<endl;
+	 cout<<s.find("a")<<endl;*/
+	 
+	 /*cout<<s.substr(3)<<endl;
+	 cout<<s.substr(1,3)<<endl;*/
+     
+     cout<<s2.length()<<endl;	
+}
